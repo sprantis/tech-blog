d131911cd232e32633abcd74408756c214e06826
Sarah Prantis <sprantis@gmail.com>
2022-10-12 17:52:14 -0400

    seed files

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+// Code referenced from Module 14 - Mini Project
+
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json')
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
