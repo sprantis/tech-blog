2d6967808019b589ff1dbd2fdde00b64f2b31225
Sarah Prantis <sprantis@gmail.com>
2022-10-14 21:55:28 -0400

    update server file

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 // TODO
 
-// WHEN I am idle on the site for more than a set time
-// THEN I am able to view comments but I am prompted to log in again before I can add, update, or delete comments
-
 // Ensure that page reloads if alert comes in when user is auto signed out
 
 // Notes: 
